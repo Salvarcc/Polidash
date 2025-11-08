50c3e424f60ef14eb7856e28ca107930c944e2ee
SamuelR2107 <samirodri2017@gmail.com>
2025-11-08 03:46:28 -0500

    salvadore perra

--- Polidash/Figura.h
+++ Polidash/Figura.h
@@ -1,93 +1,100 @@
-#pragma once
-
-using namespace System;
-using namespace System::Drawing;
-using namespace std;
+	#pragma once
+	#include "conio.h"
+#include <cstdlib> 
 
 
-class Figura
-{
-public:
-	Figura();
-	Figura(int x, int y, int velocidad, int lados, int numeroCentral, int color);
+	using namespace System;
+	using namespace System::Drawing;
+	using namespace std;
 
 
-	~Figura();
-
-	virtual void dibujar(Graphics ^canvas) = 0;
-	virtual void mover() = 0;
-	virtual void borrar(Graphics^ canvas) = 0;
-
-
-	void setX(int x);
-	void setY(int y);
-
-	int getLados();
-	int getX();		
-	int getY();
+	class Figura
+	{
+	public:
+		Figura();
+		Figura(int x, int y,int ancho,int alto, int lados, int numeroCentral, int color);
 
 
+		~Figura();
 
-private:
+		virtual void dibujar(Graphics ^canvas) = 0;
+		virtual void mover() = 0;
+		virtual void borrar(Graphics^ canvas) = 0;
 
-	int x;
-	int y;
-	int velocidad;
-	int lados;
-	int numeroCentral;
-	int color;
+
+		void setX(int x);
+		void setY(int y);
+
+		int getLados();
+		int getX();		
+		int getY();
 
 
 
+	protected:
 
-};
-
-Figura::Figura()
-{
-}
-
- Figura::Figura(int x, int y, int velocidad, int lados, int numeroCentral, int color)
-{
-	 this->x = x;
-	 this->y = y;
-	 this->lados = lados;
-	 this->numeroCentral = numeroCentral;
-	 this->velocidad = velocidad;
-	 this->color = color;
+		int x;
+		int y;
+		int velocidad;
+		int lados;
+		int numeroCentral;
+		int color;
+		int ancho;
+		int alto;
 
 
 
 
-}
+	};
 
-Figura::~Figura()
-{
-}
+	Figura::Figura()
+	{
+	}
 
- void Figura::setX(int x)
-{
-	 this->x = x;
+	 Figura::Figura(int x, int y,int ancho,int alto , int lados, int numeroCentral, int color)
+	{
+		 this->x = x;
+		 this->y = y;
+		 this->lados = lados;
+		 this->numeroCentral = numeroCentral;
+		 velocidad= rand() % (10 - 5 + 1) + 10;
+		 this->color = color;
+		 this->ancho = ancho;
+		 this->alto = alto;
 
 
-}
 
-  void Figura::setY(int y)
- {
-	  this->y = y;
 
- }
+	}
 
-int Figura::getLados()
-{
-	return lados;
-}
+	Figura::~Figura()
+	{
+	}
 
- int Figura::getX()
-{
-	return x;
-}
+	 void Figura::setX(int x)
+	{
+		 this->x = x;
 
-  int Figura::getY()
- {
-	 return y;
- }
+
+	}
+
+	  void Figura::setY(int y)
+	 {
+		  this->y = y;
+
+	 }
+
+	int Figura::getLados()
+	{
+		return lados;
+	}
+
+	 int Figura::getX()
+	{
+		return x;
+	}
+
+	  int Figura::getY()
+	 {
+		 return y;
+	 }
---
+++ Polidash/Jugador.h
@@ -0,0 +1,91 @@
+#pragma once
+#include "Figura.h"
+#include "conio.h"
+
+
+class Jugador : public Figura
+
+{
+public:
+	Jugador();
+	~Jugador();
+
+	void dibujar(Graphics^ canvas) override;
+	void borrar(Graphics^ canvas) override;
+	void mover() override;
+
+	void moverWASD();
+
+	
+private:
+
+	
+
+};
+
+Jugador::Jugador() : Figura (100,300,100,100, 3, 0, rand() % (3 - 1 + 1)+1)
+{
+
+
+}
+
+Jugador::~Jugador()
+{
+}
+
+ void Jugador::dibujar(Graphics^ canvas)
+{
+
+	 Pen^ lapiz = gcnew Pen(Color::Red);
+
+	 Point p1 = Point(x, y + alto);
+	 Point p2 = Point(x + ancho / 2, y);
+	 Point p3 = Point(x + ancho, y + alto);
+
+	 array<Point>^ points = { p1,p2,p3 };
+
+	 canvas->DrawPolygon(lapiz, points);
+
+}
+
+  void Jugador::borrar(Graphics^ canvas)
+ {
+
+	  Pen^ lapiz = gcnew Pen(Color::WhiteSmoke);
+
+	  Point p1 = Point(x, y + alto);
+	  Point p2 = Point(x + ancho / 2, y);
+	  Point p3 = Point(x + ancho, y + alto);
+
+	  array<Point>^ points = { p1,p2,p3 };
+
+	  canvas->DrawPolygon(lapiz, points);
+
+ }
+
+   void Jugador::mover()
+  {
+  }
+
+   void Jugador::moverWASD()
+  {
+	   if (_kbhit()) {
+
+		   char tecla = getch();
+
+		   switch (tecla) {
+
+
+		   case 72: y  -= velocidad; break;
+		   case 80: y  += velocidad; break;
+		   case 75: x  -= velocidad; break;
+		   case 77: x  += velocidad; break;
+
+		   }
+
+
+	   }
+
+  }
+
+
