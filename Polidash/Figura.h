cb73f4a4a3e56cd86731b6e349e7561df02c68e3
SamuelR2107 <samirodri2017@gmail.com>
2025-11-08 02:12:30 -0500

    perra

--- Polidash/Figura.h
+++ Polidash/Figura.h
@@ -9,11 +9,22 @@ class Figura
 {
 public:
 	Figura();
+	Figura(int x, int y, int velocidad, int lados, int numeroCentral, int color);
+
+
 	~Figura();
 
 	virtual void dibujar(Graphics ^canvas) = 0;
 	virtual void mover() = 0;
+	virtual void borrar(Graphics^ canvas) = 0;
 
+
+	void setX(int x);
+	void setY(int y);
+
+	int getLados();
+	int getX();		
+	int getY();
 
 
 
@@ -35,6 +46,48 @@ Figura::Figura()
 {
 }
 
+ Figura::Figura(int x, int y, int velocidad, int lados, int numeroCentral, int color)
+{
+	 this->x = x;
+	 this->y = y;
+	 this->lados = lados;
+	 this->numeroCentral = numeroCentral;
+	 this->velocidad = velocidad;
+	 this->color = color;
+
+
+
+
+}
+
 Figura::~Figura()
 {
 }
+
+ void Figura::setX(int x)
+{
+	 this->x = x;
+
+
+}
+
+  void Figura::setY(int y)
+ {
+	  this->y = y;
+
+ }
+
+int Figura::getLados()
+{
+	return lados;
+}
+
+ int Figura::getX()
+{
+	return x;
+}
+
+  int Figura::getY()
+ {
+	 return y;
+ }
---
+++ Polidash/PowerUp.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class Power : public Figura ()
+{
+public:
+	Power ();
+	~Power ();
+
+private:
+
+};
+
+Power ::Power ()
+{
+}
+
+Power ::~Power ()
+{
+}
